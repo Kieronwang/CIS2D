8b0ff8b082af0b64fbf8999e1f76ffb2099bcaaf
Kieronwang <38148291+Kieronwang@users.noreply.github.com>
2018-05-11 18:03:54 -0700

    Create Exercise14-01.java

---
+++ Coursework/Exercise14-01.java
@@ -0,0 +1,40 @@
+package com.example.java;
+import java.io.*;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.err.println("In main: calling Method1.");
+        Method1();
+        System.err.println("In main: returned from Method1.");
+    }
+
+    public static void Method1()
+    {
+        try
+        {
+            System.err.println("\tIn Method1: calling Method2.");
+            Method2();
+            System.err.println("\tIn Method1: returned from Method2.");
+        }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public static void Method2() throws FileNotFoundException
+    {
+        System.err.println("\t\tIn Method2: calling Method3.");
+        Method3();
+        System.err.println("\t\tIn Method2: returned from Method3.");
+    }
+
+    public static void Method3() throws FileNotFoundException
+    {
+        System.err.println("\t\t\tIn Method3: Entering.");
+        RandomAccessFile in = new RandomAccessFile("products.ran", "rw");
+        System.err.println("\t\t\tIn Method3: Exiting.");
+    }
+}
